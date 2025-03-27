4b91adcb81a41274e840e268a35d5dbbd6f7297c
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-03-27 14:46:13 +0600

    register route..

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const path = require("path");
+
 const app = express();
 const port = 3000;
 
+const userModel = require("./models/user");
+const postModel = require("./models/post");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
+});
+
+app.post("/register", async (req, res) => {
+  let { name, username, age, email, password } = req.body;
+  let user = await userModel.findOne({ email });
+  if (user) return res.status(400).send("User already exists");
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let user = await userModel.create({
+        name,
+        username,
+        age,
+        email,
+        password: hash,
+      });
+      let token = jwt.sign({ email, userid: user._id }, "secret");
+      res.cookie("token", token);
+      res.send("registered!");
+    });
+  });
 });
 
 app.listen(port, () => {
